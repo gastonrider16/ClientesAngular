c757e49c52f5a234692ba397e5cfa75f3e77418f
gastonrider16 <gastonrider16@gmail.com>
2021-08-05 11:36:27 -0300

    Version 1

---
+++ app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Listado de empleados';
+  empleados:Empleado[]=[
+    new Empleado("Juan","Gomez","Programador",600000),
+    new Empleado("Gastón","Ramirez","Programador",40000)
+  ];
+}
