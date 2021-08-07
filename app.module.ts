04ac4d4a6019e19fe1c532ff5476259f91a87751
gastonrider16 <gastonrider16@gmail.com>
2021-08-06 22:55:13 -0300

    Version 1.2 Providers

--- app.module.ts
+++ app.module.ts
@@ -3,17 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EmpleadoHijoCComponent } from './src/app/empleado-hijo-c/empleado-hijo-c.component';
+import { EmpleadoHijoCComponent } from './empleado-hijo-c/empleado-hijo-c.component';
+import { CaracteristicasEmpleadoComponent } from './caracteristicas-empleado/caracteristicas-empleado.component';
+import { ServicioEmpleadosService } from './servicio-empleados.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadoHijoCComponent
+    EmpleadoHijoCComponent,
+    CaracteristicasEmpleadoComponent
   ],
   imports: [
     BrowserModule, FormsModule
   ],
-  providers: [],
+  providers: [ServicioEmpleadosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
